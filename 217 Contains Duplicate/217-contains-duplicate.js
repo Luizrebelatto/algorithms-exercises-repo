a2870be2bdbd0c06fd4aab27d64356502c99e3cc
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-03-22 18:23:42 -0300

    fix: add reserved word 'let'

--- 217 Contains Duplicate/217-contains-duplicate.js
+++ 217 Contains Duplicate/217-contains-duplicate.js
@@ -1,7 +1,7 @@
 var containsDuplicate = function(nums) {
     const hashArray = new Set();
 
-    for(i=0;i <= nums.length; i++){
+    for(let i=0; i < nums.length; i++){
         if(hashArray.has(nums[i])) return true
         hashArray.add(nums[i])
     }
