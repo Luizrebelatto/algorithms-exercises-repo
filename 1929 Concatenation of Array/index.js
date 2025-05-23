438e5dff4004b48fc90420dfeabc233d3c82e2c8
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-05-23 07:57:35 -0300

    new exercise 1929 concatenation of array

---
+++ 1929 Concatenation of Array/index.js
@@ -0,0 +1,9 @@
+var getConcatenation = function(nums) {
+    const ans = []
+    for(let i = 0; i < 2; i++){
+        for(let j=0; j < nums.length; j++){
+                ans.push(nums[j])
+        }
+    }
+    return ans
+};
